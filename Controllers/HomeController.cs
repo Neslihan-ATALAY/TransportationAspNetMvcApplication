8ae3b8642f2548b3c23e0cb1fa476647cd14181a
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2023-09-23 21:00:38 +0300

    Update HomeController.cs
    Home Controller - add authorization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace TransportationMvc2Project.Controllers
     [HandleError]
     public class HomeController : Controller
     {
+        [Authorize(Roles = "KULLANICI, ŞİRKET")]
         public ActionResult Index()
         {
             ViewData["Message"] = "Transportation Application - Nakliyat Uygulaması";
